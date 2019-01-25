fec317d42423786d700d94b7913df345a46944e6
Yannick Simchen <yannick.simchen@student.mi.hs-rm.de>
2019-01-25 11:06:55 +0100

    first commit - django-structure & rquirements.txt

---
+++ myRDB_Project/myRDB_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyrdbAppConfig(AppConfig):
+    name = 'myRDB_app'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yannic~1\pychar~1\myrdb\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
