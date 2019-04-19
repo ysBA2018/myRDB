ab29c43f935055cf3b94a442d78c2b2e48aeea4a
Yannick Simchen <yannick.simchen@student.mi.hs-rm.de>
2019-04-19 17:57:36 +0200

    Dockerfile change: ENV variablen

--- myRDB_Project/myRDB_Project/settings.py
+++ myRDB_Project/myRDB_Project/settings.py
@@ -155,3 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
