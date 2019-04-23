959dde6d4d4426c3272e113d313c9fb04dc065f6
Yannick Simchen <yannick.simchen@student.mi.hs-rm.de>
2019-04-23 14:45:21 +0200

    für API-http-requests ->http://148.100.245.159:8000 debug False static_root

--- myRDB_Project/myRDB_Project/settings.py
+++ myRDB_Project/myRDB_Project/settings.py
@@ -160,5 +160,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
